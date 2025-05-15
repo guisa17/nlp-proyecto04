ec162dfc5ab7e3b287a520a2213db48d8f822883
Guillermo <guillermo.salcedo.a@uni.pe>
2025-05-14 21:16:17 -0500

    feat: funcion para guardar toy data

--- src/utils.py
+++ src/utils.py
@@ -1,5 +1,6 @@
 import random
-
+import json
+import os
 
 def generate_toy_data(N=10000, max_digits=2):
     """
@@ -13,6 +14,18 @@ def generate_toy_data(N=10000, max_digits=2):
         inp = f"{a}+{b}"
         tgt = str(a + b)
         data.append((inp, tgt))
-        
+
     cut = int(0.8 * N)
     return data[:cut], data[cut:]
+
+
+def save_toy_data(train_data, val_data, path="data/toy_seq2seq.json"):
+    """
+    Guardamos el dataset en un archivo JSON
+    """
+    data = {
+        "train": [{"input": inp, "target": tgt} for inp, tgt in train_data],
+        "val": [{"input": inp, "target": tgt} for inp, tgt in val_data],
+    }
+    with open(path, "w") as f:
+        json.dump(data, f, indent=4)
